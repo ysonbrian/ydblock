704325c84a130583cccac02864026088a3628d0c
ysonbrian <znlsakxls@gmail.com>
2021-11-09 16:02:04 +0900

    Resetting site

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
 import Header from './components/Header/Header';
+import About from './components/About/About';
 import Main from './components/Main/Main';
-
+import { Switch, Route } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
       <Header />
-      <Main />
+      <Switch>
+        <Route exact path="/" component={Main} />
+        <Route path="/about" component={About} />
+        <Route path="/main" component={Main} />
+      </Switch>
     </div>
   );
 }
--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,16 +1,25 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Header.css';
 const Header = () => {
   return (
     <div className="header__container">
-      <h1>
-        <a href="#">ysonbrian blog</a>
-      </h1>
-      <nav className="nav__container">
-        <h2>Articles</h2>
-        <h2>About</h2>
-      </nav>
+      <Link to="/">
+        <h1>ysonbrian</h1>
+      </Link>
+      <ul className="nav__container">
+        <li>
+          <Link to="/about">
+            <h2>About</h2>
+          </Link>
+        </li>
+        <li>
+          <Link to="/main">
+            <h2>Articles</h2>
+          </Link>
+        </li>
+      </ul>
     </div>
   );
 };
